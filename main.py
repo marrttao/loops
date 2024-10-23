0b5a75ebdd0f4d1254254c77f54a42870577c333
marrttao <marrttaoburberry@gmail.com>
2024-10-23 18:42:56 +0300

    ex_1 (not completed)

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    pass
+def simple_number(a, b):
+    for num in range(a, b + 1):
+        is_simple = True
+        for j in range(2, num):
+            if num % j == 0:
+                is_simple = False
+                break
+        if is_simple and num > 1:
+            print(num)
+if __name__ == 'main':
+    a = int(input())
+    b = int(input())
+    simple_number(1, 25)
