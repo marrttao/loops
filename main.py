a61d6a007aa55edc964fae2e94d57acbeaf035a1
marrttao <marrttaoburberry@gmail.com>
2024-10-23 19:51:32 +0300

    complete task_3

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def ranged_multiplication_table(start, end):
+    for i in range(start, end + 1):
+        for j in range(start, 10+1):
+            print(f"{i} * {j} = {i * j}", end = " ")
+        print("\n")
+
 if __name__ == '__main__':
-    pass
+    a = int(input())
+    b = int(input())
+    ranged_multiplication_table(a, b)
+
