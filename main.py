6c3644f15875cca2313f0793853598d1ba09ec3b
marrttao <marrttaoburberry@gmail.com>
2024-10-23 19:40:37 +0300

    ex_1 problem was with __name__ xD

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+def multiplication_table(start, end):
+    for i in range(start, end + 1):
+        for j in range(start, end + 1):
+            print(f"{i} * {j} = {i * j}", end = " ")
+        print("\n")
+
 if __name__ == '__main__':
-    pass
+    multiplication_table(1, 10)
