d3c0d0695d1a75f48ddc131ca2beeea596ef0631
marrttao <marrttaoburberry@gmail.com>
2024-10-24 21:45:34 +0300

    added succesfull unittest

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def multiplication_table(start, end):
+    result = []
     for i in range(start, end + 1):
+        row = []
         for j in range(start, end + 1):
+            row.append(f"{i} * {j} = {i * j}")
             print(f"{i} * {j} = {i * j}", end = " ")
+            result.append(row)
         print("\n")
+    return result
 
 if __name__ == '__main__':
+
     multiplication_table(1, 10)
