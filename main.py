cb7e8cd87ede69acac471c4c5280291167d33e3b
marrttao <marrttaoburberry@gmail.com>
2024-10-25 15:59:59 +0300

    completed ex_2

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+def chess_desk(large_of_square):
+    if large_of_square <= 0:
+
+        for _ in range(6):
+            print(" ")
+        return
+    rows = 6
+    columns = 8
+    for i in range(rows):
+        for j in range(columns // 2):
+            print('*' * large_of_square, end=' ')
+            print('-' * large_of_square, end=' ')
+        print()
 if __name__ == '__main__':
-    pass
+    size = int(input())
+    chess_desk(size)
