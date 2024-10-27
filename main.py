b4708c29c54948241c0abf12babeaf3e46ce028f
marrttao <marrttaoburberry@gmail.com>
2024-10-27 17:47:49 +0200

    made it faster

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-#сделал возможность выбирать размер ромба при проверке увидите
-
-def rhombus(len_1, len_2):
-    print("\n")
-    for i in range(len_1):
-        for j in range(len_2):
-            if j >= len_2 - i - 1:
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-        for j in range(len_2):
-            if j+1 <= i:
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-
-        print()
-
-    for i in range(len_1 - 1, -1, -1):
-        for j in range(len_2):
-            if j-1 >= len_2 - i - 1:
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-        for j in range(len_2):
-            if j+2 <= i:
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-
-        print()
-
-
+def rhombus(size):
+    for i in range(size):
+        print(' ' * (size - i - 1) + '*' * (2 * i + 1))
+    for i in range(size - 2, -1,-1):
+        print(' ' * (size - i - 1) + '*' * (2 * i + 1))
 if __name__ == '__main__':
-    len_1 = int(input("Write a len_1 of rhombus"))
-    len_2 = int(input("Write a len_2 of rhombus"))
-    rhombus(len_1, len_2)
+    size = int(input("Write a size of rhombus "))
+    rhombus(size)
